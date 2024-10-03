a94456ebd94287a62214c68b3c49fb2287b62bbc
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-03 16:01:38 +0700

    Configure Sepolia Chain on RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,30 @@
 import { Outfit } from 'next/font/google'
 import React from 'react'
 import { PrivyProvider } from '@privy-io/react-auth'
+import { defineChain } from 'viem'
 
 import './globals.css'
 
 const font = Outfit({ subsets: ['latin'] })
+
+const Sepolia = defineChain({
+	id: 11155111,
+	name: 'Sepolia test network',
+	network: 'Sepolia test network',
+	nativeCurrency: {
+		decimals: 18,
+		name: 'Sepolia test network',
+		symbol: 'SepoliaETH',
+	},
+	rpcUrls: {
+		default: {
+			http: ['https://sepolia.infura.io/v3/'],
+		},
+	} as any,
+	blockExplorers: {
+		default: { name: 'Explorer', url: 'https://sepolia.etherscan.io' },
+	},
+}) as any
 
 export default function RootLayout({
 	children,
@@ -27,6 +47,8 @@ export default function RootLayout({
 						embeddedWallets: {
 							createOnLogin: 'users-without-wallets',
 						},
+						defaultChain: Sepolia,
+						supportedChains: [Sepolia],
 					}}
 				>
 					{children}
